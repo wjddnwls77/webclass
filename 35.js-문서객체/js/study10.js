b7b0b2f01d0ce391342578b75765636eab7bfbc2
Jeong woo jin <wjddnwls77@ewhain.net>
2023-05-25 18:11:12 +0900

    subvawerf

--- 35.js-문서객체/js/study10.js
+++ 35.js-문서객체/js/study10.js
@@ -1,22 +1,57 @@
 window.onload =function(){
 
-    let slider1 = new Swiper(".station1",{
-        loop: true,
-        slidesPerView:1.5,
+    let station1 = new Swiper(".station1",{
+        loop:true,
+        direction:'horizontal',
+        // slidesPerView:3,//슬라이드 개수의 반만큼 최대 적용 가능.
+        // spaceBetween:40,
         pagination: {
-            el: ".swiper-pagination",
-            dynamicBullets: true,
+            el: '.swiper-pagination',
+            type: 'bullets',
           },
-        
-
-    })
+          navigation: {
+            nextEl: ".csNextBtn",
+            prevEl: ".csPrevBtn",
+          },
+          autoplay:{
+            delay:1000,
+            // pauseOnMouseEnter:true
+        },
+        speed:1000,
+        breakpoints:{
+            320:{ 
+                slidesPerView:1,
+                spaceBetween:0
+            },
+            768:{
+                slidesPerView:2,
+                spaceBetween:10
+            },
+            1024:{
+                slidesPerView:3,
+                spaceBetween:20
+            }
+        }
     
-    let slider2 = new Swiper(".station2",{
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",}
-
+    })
+    document.querySelector(".station1").addEventListener("mouseover",function(){
+        station1.autoplay.stop()
+    })
+    document.querySelector(".station1").addEventListener("mouseout",function(){
+        station1.autoplay.start()
     })
 
+    
+  
 
+
+
+
+
+
+
+
+
+
+    
 }
