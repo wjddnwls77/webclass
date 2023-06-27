8f1f231470b77f4b0977d5ec849afe03fe4f376b
Jeong woo jin <wjddnwls77@ewhain.net>
2023-06-27 18:14:02 +0900

    Wfdasdf

---
+++ svg/js/study2.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    $(".btn").click(function(){
+        if($(this).parent().hasClass("on")==false){
+            $(this).parent().addClass("on")
+            let per =  Number($(this).parent().find(".per").text())
+            let circle = $(this).parent().find("circle ")
+            // circle.css("stroke-dashoffset",314-(314*(per/100)))
+       
+    
+            let percetTag = $(this).parent().find(".per")
+            let count = 0;
+            let timer = setInterval(function(){
+                count++ 
+                percetTag.text(count+"%")
+                circle.css("stroke-dashoffset",314-(314*(per/100)))
+                if(count>=per){
+                    clearInterval(timer)
+                }
+            },30)
+        }
+       
+})
+
+})
